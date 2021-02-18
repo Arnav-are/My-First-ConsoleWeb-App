97e7007e1034b22765044f8fa3582ba702d6c574
Arnav-are <73885146+Arnav-are@users.noreply.github.com>
2021-02-18 08:35:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("My First Console Web App")
+var first_name="Arnav";
+console.log (first_name);   
+var last_name="Khandelwal";
+console.log(last_name);
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+var power=Math.pow(4,2);
+console.log(power);
+var sq=Math.sqrt(10);
+console.log(sq);
+var minimum=Math.min(21,122);
+console.log(minimum);
+var maximun=Math.max(233,2143);
+console.log(maximun)
